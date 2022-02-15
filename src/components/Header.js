d7c92e08373ff12d62511ecf8b9be72904e3b9cc
Si Yang Cui <siyangcui@gmail.com>
2022-02-15 16:42:53 +1100

    secondary banner for categories

--- src/components/Header.js
+++ src/components/Header.js
@@ -57,7 +57,21 @@ const Header = () => {
       </div>
 
       {/* Bottom Nav */}
-      <div></div>
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="hidden lg:inline-flex link">Electronics</p>
+        <p className="hidden lg:inline-flex link">Food and Grocery</p>
+        <p className="hidden lg:inline-flex link">Prime</p>
+        <p className="hidden lg:inline-flex link">Buy Again</p>
+        <p className="hidden lg:inline-flex link">Shopper Toolkit</p>
+        <p className="hidden lg:inline-flex link">Health & Personal Care</p>
+      </div>
     </header>
   );
 };
